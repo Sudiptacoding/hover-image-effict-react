d13ec2638307af5350da00720f8647363c56e32b
Sudiptacoding <you@example.comsudiptabiswas506@gmail.com>
2022-11-19 23:53:30 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import HoverImg from './Components/HoverImg/HoverImg';
+
+function App() {
+  return (
+    <div>
+      <HoverImg></HoverImg>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/HoverImg/HoverImg.js
@@ -0,0 +1,75 @@
+import React from 'react';
+import './HoverImg.css';
+
+const HoverImg = () => {
+    return (
+        <div className='dis'>
+            <div className="outer">
+                <div className="one">
+                    <div>Hover Me</div>
+                </div>
+                <div className="two">
+                    <div>Thanks For Hover</div>
+                </div>
+            </div>
+            <div className="outer">
+                <div className="one">
+                    <div>Hover Me</div>
+                </div>
+                <div className="two">
+                    <div>Thanks For Hover</div>
+                </div>
+            </div>
+            <div className="outer">
+                <div className="one">
+                    <div>Hover Me</div>
+                </div>
+                <div className="two">
+                    <div>Thanks For Hover</div>
+                </div>
+            </div>
+            <div className="outer">
+                <div className="one">
+                    <div>Hover Me</div>
+                </div>
+                <div className="two">
+                    <div>Thanks For Hover</div>
+                </div>
+            </div>
+            <div className="outer">
+                <div className="one">
+                    <div>Hover Me</div>
+                </div>
+                <div className="two">
+                    <div>Thanks For Hover</div>
+                </div>
+            </div>
+            <div className="outer">
+                <div className="one">
+                    <div>Hover Me</div>
+                </div>
+                <div className="two">
+                    <div>Thanks For Hover</div>
+                </div>
+            </div>
+            <div className="outer">
+                <div className="one">
+                    <div>Hover Me</div>
+                </div>
+                <div className="two">
+                    <div>Thanks For Hover</div>
+                </div>
+            </div>
+            <div className="outer">
+                <div className="one">
+                    <div>Hover Me</div>
+                </div>
+                <div className="two">
+                    <div>Thanks For Hover</div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default HoverImg;
